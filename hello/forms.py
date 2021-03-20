882d92f35b5898e43823a4f9c3cf571a734f223c
maicindy15 <57304289+maicindy15@users.noreply.github.com>
2021-03-19 23:51:19 -0500

    Add files via upload

--- hello/forms.py
+++ hello/forms.py
@@ -2,9 +2,9 @@ from django import forms
 from localflavor.us.forms import USStateSelect
 
 class profileManage(forms.Form):
-    fullName=forms.CharField(label='full-name',max_length=50)
-    address1=forms.CharField(label='add1',max_length=100)
-    address2=forms.CharField(label='add2',max_length=100,required=False)
-    city=forms.CharField(label='city',max_length=100)
+    fullName=forms.CharField(max_length=50)
+    address1=forms.CharField(max_length=100)
+    address2=forms.CharField(max_length=100,required=False)
+    city=forms.CharField(max_length=100)
     states = forms.CharField(widget=USStateSelect)
 
