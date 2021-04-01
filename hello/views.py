d796c2759267160ad29efc4766702a6bcfa2a560
jantamayo <62530922+jantamayo@users.noreply.github.com>
2021-04-01 05:01:16 -0500

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -10,8 +10,9 @@ from .forms import registerForm
 from .import forms
 from django.template.loader import get_template
 from django.template import Context
-from .models import Register
+from .models import Register, Quote
 import logging
+
 
 def home(request):
     return render(request,'index.html')
@@ -52,17 +53,37 @@ def form(request):
     if request.method == 'POST':
         form = forms.FuelQuoteForm(request.POST)
         if form.is_valid():
-            # SAVE TO DB
+            newQuote = form.save(commit=False)
+            newQuote.fullAddress = '1234 Take Address From Profile'
+                # NEED TO TAKE ADDRESS FROM PROFILE
+            newQuote.pricePerGallon = 0.99
+            newQuote.totalDue = 999.99
+                # DUMMY VALUES FOR pricePerGallon and totalDue, NEED TO APPLY PRICING MODULE
+            newQuote.save()
             return redirect('quote')
     else:
         form = forms.FuelQuoteForm()
     return render(request,'form.html',{'form':form})
 
 @login_required
+@csrf_protect
 def profile(request):
-    return render(request, 'profile-management.html')
+    if request.method == 'POST':
+        form = forms.profileManage(request.POST)
+        if form.is_valid():
+            # SAVE TO DB
+            profileDetails = form.save()
+            return redirect('form')
+    else:
+        form = forms.profileManage()
+    return render(request, 'profile-management.html',{'form':form})
+
+
 def history(request):
-    return render(request, 'fuelQuoteHistory.html')
+    context = {
+        'posts': Quote.objects.all()
+    }
+    return render(request, 'fuelQuoteHistory.html', context)
 
 def quote(request):
     return render(request,"quote.html")
