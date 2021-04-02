7caf61ca2d263588235cb0eed919bac11685add2
jantamayo <62530922+jantamayo@users.noreply.github.com>
2021-04-02 04:12:21 -0500

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -48,17 +48,18 @@ def loginpage(request):
 
     return render(request,'login.html',{'form':form, 'title':'login'})
 
+@login_required
 @csrf_protect
 def form(request):
     if request.method == 'POST':
         form = forms.FuelQuoteForm(request.POST)
         if form.is_valid():
             newQuote = form.save(commit=False)
-            newQuote.fullAddress = '1234 Take Address From Profile'
-                # NEED TO TAKE ADDRESS FROM PROFILE
-            newQuote.pricePerGallon = 0.99
-            newQuote.totalDue = 999.99
-                # DUMMY VALUES FOR pricePerGallon and totalDue, NEED TO APPLY PRICING MODULE
+            currUser = request.user.profile
+            fullAddress = f'{currUser.address1}, {currUser.city}, {currUser.state} {currUser.zipcode}'
+            newQuote.fullAddress = fullAddress
+            newQuote.pricePerGallon = 1.50 # Hardcoded for now, implement pricing module
+            newQuote.totalDue = newQuote.pricePerGallon * newQuote.gallonsReq
             newQuote.save()
             return redirect('quote')
     else:
@@ -69,10 +70,12 @@ def form(request):
 @csrf_protect
 def profile(request):
     if request.method == 'POST':
+        # form = forms.profileManage(request.POST, instance=request.user.profile)
         form = forms.profileManage(request.POST)
         if form.is_valid():
-            # SAVE TO DB
-            profileDetails = form.save()
+            newProfile = form.save(commit=False)
+            newProfile.user = request.user
+            newProfile.save()
             return redirect('form')
     else:
         form = forms.profileManage()
